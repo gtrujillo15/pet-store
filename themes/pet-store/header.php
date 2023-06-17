f8a0b9fad6386811b530f0c0fceee85a14a2f0d6
Gabriela T <gtrujillo15@gmail.com>
2023-06-17 12:06:30 -0700

    feat(logo):create header styling and logo

--- themes/pet-store/header.php
+++ themes/pet-store/header.php
@@ -35,34 +35,28 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'pet-store' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$pet_store_description = get_bloginfo( 'description', 'display' );
-			if ( $pet_store_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $pet_store_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+		<section class="site-header-wrapper d-flex flex-row justify-content-between align-items-center">
+			<div class="site-branding d-flex flex-row">
+				<?php the_custom_logo(); ?>
+					<h1 class="website-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php
+				$pet_store_description = get_bloginfo( 'description', 'display' );
+				if ( $pet_store_description || is_customize_preview() ) :
+					?>
+					<p class="site-description"><?php echo $pet_store_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+				<?php endif; ?>
+			</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'pet-store' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
+			<nav id="site-navigation" class="main-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'pet-store' ); ?></button>
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+					)
+				);
+				?>
+			</nav><!-- #site-navigation -->
+		</section>
 	</header><!-- #masthead -->
